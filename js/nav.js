10dab5343c8d66b21224c479416cc11c2f6e92b3
Ubaid Badar <m.ubaidbadar@gmail.com>
2019-11-09 23:52:52 +0500

    show_nav_on_page_load

--- js/nav.js
+++ js/nav.js
@@ -3,11 +3,11 @@ function showNav(menu) {
     if (check) {
         document.getElementById('nav').className = 'openNav';
         menu.className = 'openNavMenu';
-        document.body.style.marginLeft = "288px";
+        document.body.style.marginLeft = 0;
     } else{
         document.getElementById('nav').className = '';
         menu.className = '';
-        document.body.style.marginLeft = 0;
+        document.body.style.marginLeft = "288px";
     }
     check = !check;
 }
