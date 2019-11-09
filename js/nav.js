480c92c2273ce41c0f23db91ed04e3ba2e4294ac
Ubaid Badar <m.ubaidbadar@gmail.com>
2019-11-09 23:33:34 +0500

    add_collapse_ability

--- js/nav.js
+++ js/nav.js
@@ -3,9 +3,11 @@ function showNav(menu) {
     if (check) {
         document.getElementById('nav').className = 'openNav';
         menu.className = 'openNavMenu';
+        document.body.style.marginLeft = "288px";
     } else{
         document.getElementById('nav').className = '';
-        menu.className = ''
+        menu.className = '';
+        document.body.style.marginLeft = 0;
     }
     check = !check;
 }
