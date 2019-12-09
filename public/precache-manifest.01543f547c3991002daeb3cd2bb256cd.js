add4c4d5638730f6f9858266ed348c59e4e51665
Ubaid Badar <m.ubaidbadar@gmail.com>
2019-12-09 15:02:10 +0500

    removed_unecessary_things

---
+++ public/precache-manifest.01543f547c3991002daeb3cd2bb256cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eab7bb9577a2d3e7249b7e7dc925bb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89202082890e34b41bbb",
+    "url": "/static/css/main.0d2a9357.chunk.css"
+  },
+  {
+    "revision": "e4cb4a0536f2ba68a6b8",
+    "url": "/static/js/2.d840f7c0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.d840f7c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89202082890e34b41bbb",
+    "url": "/static/js/main.db22bf7f.chunk.js"
+  },
+  {
+    "revision": "8b7004f2d9a6b2fcdbf2",
+    "url": "/static/js/runtime-main.f3205b98.js"
+  }
+]);
